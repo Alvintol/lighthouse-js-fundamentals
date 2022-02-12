b4eeb58180897db7be5b9f2238ac1fd79e94cbf7
Alvintol <alvintol+github@hotmail.com>
2022-02-12 19:04:15 +0000

    update to sort numbers from least to greatest

--- lighthouse-js-fundamentals/concatenate-arrays.js
+++ lighthouse-js-fundamentals/concatenate-arrays.js
@@ -4,14 +4,18 @@
  - The function should, when given two arrays, 
  concatenate the arrays together
 */
+
+// ** NEW CONDITION
+// make sure data is sorted from lowest to highest
 const concat = function (data1, data2) {
   for (let a = 0; a < data2.length; a++){
     data1.push(data2[a]);
+    data1.sort((b, c) => b - c);
   }
-  return data1;
+  return data1
 }
 
-console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 1, 2, 3 ], [ 4, 6, 5 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
 console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
 console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
 console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
