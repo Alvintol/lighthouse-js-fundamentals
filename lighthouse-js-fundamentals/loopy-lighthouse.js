745f8523cd7b346ebaa598f7484affd0c908b2d5
Alvintol <alvin.tol@hotmail.com>
2022-03-07 23:59:35 +0000

    simple code format

--- lighthouse-js-fundamentals/loopy-lighthouse.js
+++ lighthouse-js-fundamentals/loopy-lighthouse.js
@@ -43,7 +43,7 @@ representing the multiples you want to replace with words.
 //   count++;
 // }
 let count = 0;
-const loopyLighthouse = function (range, multiple, words) {
+const loopyLighthouse = function(range, multiple, words) {
   while (range[0] <= range[1]) {
     count = range[0]++;
     if (count % multiple[0] === 0 && count % multiple[1] === 0) {
@@ -51,9 +51,9 @@ const loopyLighthouse = function (range, multiple, words) {
     } else if (count % multiple[0] === 0 && count % multiple[1] !== 0) {
       count = words[0];
     } else if (count % multiple[0] !== 0 && count % multiple[1] === 0) {
-      count = words[1]
+      count = words[1];
     } console.log(count);
   }
-}
+};
 //               range    multiple    words
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
