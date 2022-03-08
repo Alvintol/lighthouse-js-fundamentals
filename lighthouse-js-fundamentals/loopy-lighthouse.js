19ffed2361ae230af2ccbb06ef036b8d3d64e79d
Alvintol <alvin.tol@hotmail.com>
2022-03-08 01:06:51 +0000

    refractored

--- lighthouse-js-fundamentals/loopy-lighthouse.js
+++ lighthouse-js-fundamentals/loopy-lighthouse.js
@@ -2,11 +2,13 @@ let count = 0;
 const loopyLighthouse = (range, multiple, words) => {
   while (range[0] <= range[1]) {
     count = range[0]++;
-    if (count % multiple[0] === 0 && count % multiple[1] === 0) {
+    const multi0 = count % multiple[0];
+    const multi1 = count % multiple[1];
+    if (multi0 === 0 && multi1 === 0) {
       count = words[0] + words[1];
-    } else if (count % multiple[0] === 0 && count % multiple[1] !== 0) {
+    } else if (multi0 === 0 && multi1 !== 0) {
       count = words[0];
-    } else if (count % multiple[0] !== 0 && count % multiple[1] === 0) {
+    } else if (multi0 !== 0 && multi1 === 0) {
       count = words[1];
     } console.log(count);
   }
